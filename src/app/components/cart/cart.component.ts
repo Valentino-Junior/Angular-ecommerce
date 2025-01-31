5cb535dccfb705054b44e94454469974cee59701
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 05:15:41 +0300

    added checkout and go back method

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -11,18 +12,26 @@ export class CartComponent implements OnInit {
   cartItems$: Observable<any[]>;
   totalPrice$: Observable<number>;
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private router: Router) {
     this.cartItems$ = this.cartService.getCartItems();
     this.totalPrice$ = this.cartService.getTotalPrice();
   }
 
   ngOnInit(): void {}
 
+  updateQuantity(productId: number, quantity: number): void {
+    this.cartService.updateQuantity(productId, quantity);
+  }
+
   removeFromCart(productId: number): void {
     this.cartService.removeFromCart(productId);
   }
 
-  clearCart(): void {
-    this.cartService.clearCart();
+  goToCheckout(): void {
+    this.router.navigate(['/checkout']);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
   }
 }
