7e8d566c2010b1c438b3cd3bbcb3132826d55dd1
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 06:01:33 +0300

    updated the categorylist and add view details

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,24 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../services/product.service';
+import { Component, type OnInit } from "@angular/core"
+import type { Observable } from "rxjs"
+import type { Category } from "../../interfaces/product.interface"
+import { ProductService } from "../../services/product.service"
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.css"],
 })
 export class CategoryListComponent implements OnInit {
-  categories: any[] = [];
+  categories$: Observable<Category[]>
 
-  constructor(private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.productService.getCategories().subscribe(
-      (data) => {
-        this.categories = data;
-      },
-      (error) => {
-        console.error('Error fetching categories:', error);
-      }
-    );
+  constructor(private productService: ProductService) {
+    this.categories$ = this.productService.getCategories()
   }
+
+  ngOnInit(): void {}
 }
+
