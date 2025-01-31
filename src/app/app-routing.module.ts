b3bf50264a7ce35d7e32711a84537ee76b57a6c3
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 04:40:31 +0300

    Added category detail path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { NgModule } from "@angular/core"
+import { RouterModule, type Routes } from "@angular/router"
+import { ProductListComponent } from "./components/product-list/product-list.component"
+import { ProductDetailComponent } from "./components/product-detail/product-detail.component"
+import { CartComponent } from "./components/cart/cart.component"
+import { CheckoutComponent } from "./components/checkout/checkout.component"
+import { OrderSuccessComponent } from "./components/order-success/order-success.component"
+import { CategoryListComponent } from "./components/category-list/category-list.component"
+import { CategoryDetailComponent } from "./components/category-detail/category-detail.component"
 
 const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductDetailComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'order-success', component: OrderSuccessComponent },
-];
+  { path: "", redirectTo: "/products", pathMatch: "full" },
+  { path: "products", component: ProductListComponent },
+  { path: "product/:id", component: ProductDetailComponent },
+  { path: "categories", component: CategoryListComponent },
+  { path: "category/:id", component: CategoryDetailComponent },
+  { path: "cart", component: CartComponent },
+  { path: "checkout", component: CheckoutComponent },
+  { path: "order-success", component: OrderSuccessComponent },
+  { path: "**", redirectTo: "/products" }, // Wildcard route for a 404 page
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
