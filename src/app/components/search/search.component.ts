7c27845120f739e9293fca62cf34b9b23e83129d
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 07:31:27 +0300

    added search method

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchTerm: string = '';
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSearch(): void {
+    this.productService.searchProducts(this.searchTerm);
+  }
 }
