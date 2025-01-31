6b0f69e713790bbbd253215f3ab7373c1bed298f
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 05:06:05 +0300

    added cart methods like clearcart, remove from cart,

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems$: Observable<any[]>;
+  totalPrice$: Observable<number>;
 
+  constructor(private cartService: CartService) {
+    this.cartItems$ = this.cartService.getCartItems();
+    this.totalPrice$ = this.cartService.getTotalPrice();
+  }
+
+  ngOnInit(): void {}
+
+  removeFromCart(productId: number): void {
+    this.cartService.removeFromCart(productId);
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart();
+  }
 }
