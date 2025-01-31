280891661cf23563887228f2752b4423f68999ff
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 04:09:30 +0300

    Added imported services and graphql module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { ProductService } from './services/product.service';
+import { CartService } from './services/cart.service';
 
 @NgModule({
   declarations: [
@@ -25,10 +27,11 @@ import { OrderSuccessComponent } from './components/order-success/order-success.
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     GraphQLModule,
-    HttpClientModule
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ProductService, CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
