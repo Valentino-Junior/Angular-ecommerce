098ec55b977b6ff7d985b0f5a37288d9124dad85
Valentino Junior <ojvalentine14@gmail.com>
2025-01-31 05:28:40 +0300

    added go home method

--- src/app/components/order-success/order-success.component.ts
+++ src/app/components/order-success/order-success.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-success',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-success.component.css']
 })
 export class OrderSuccessComponent {
+  constructor(private router: Router) {}
 
+  goToHome(): void {
+    this.router.navigate(['/']);
+  }
 }
